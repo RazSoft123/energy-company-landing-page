5f452458d59ca8db607ce4b9b90d0e4759ba02a8
RazSoft <razgroupgame@gmail.com>
2025-06-19 16:43:16 +0530

    add style to testimonials section

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,10 +1,12 @@
+import solar_review from './../assets/solar_review.jpg';
+
 export default function TestimonialSection() {
     return (
-        <section className="w-full border-2">
-            <div>
-                <h3>See how we solve <br/> problems, <span>right on target</span></h3>
+        <section className="w-full my-8">
+            <div className="w-full flex items-center justify-center mb-16">
+                <h3 className="text-center text-4xl">See how we solve <br/> problems, <span className="text-gray-400">right on target</span></h3>
             </div>
-            <div>
+            <div className="w-full flex items-center justify-center p-8">
                 <TestimonialCard />
             </div>
         </section>
@@ -13,18 +15,21 @@ export default function TestimonialSection() {
 
 function TestimonialCard() {
     return (
-        <div>
-            <div>
-                <h3>Medtronic</h3>
-                <p>We have used services form xurya for the most of our stations this is our stratgic step to continue to increse the number of solar panels uses for our stations, We are very stisfied with the services from Xurya</p>
-                <a href="">View full review</a>
+        <div className="w-[70%] bg-gray-300 rounded-xl flex p-3 gap-8">
+            <div className='p-8 flex flex-col justify-between'>
+                <div className='flex flex-col gap-4'>
+                    <h3 className="font-bold text-xl">Medtronic</h3>
+                    <p>We have used services form xurya for the most of our stations this is our stratgic step to continue to increse the number of solar panels uses for our stations, We are very stisfied with the services from Xurya</p>
+                    <a className="w-fit px-4 py-2 bg-green-500 rounded-4xl text-gray-200 text-sm" href="">View full review</a>
+                </div>
+                
 
                 <div>
-                    <p>jennifer <span>CTO EV Medtronic</span></p>
+                    <p className='text-black font-bold'>jennifer - <span className='text-slate-100'>CTO EV Medtronic</span></p>
                 </div>
             </div>
-            <div>
-                <img src="null" alt="" />
+            <div className='rounded-xl overflow-hidden'>
+                <img src={solar_review} alt="" />
             </div>
         </div>
     )
